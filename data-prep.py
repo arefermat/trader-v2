9ceaa644d60b2ddaa80887f0c81910ca4718c9f4
Alex Refermat <73394181+arefermat@users.noreply.github.com>
2024-11-06 11:53:06 -0600

    Create data-prep.py

---
+++ data-prep.py
@@ -0,0 +1,30 @@
+import pandas
+import schedule
+import matplotlib
+import keyboard
+import numpy
+
+def prep_data_for_graph():
+  pass
+
+def fetch_data():
+  pass
+
+class gragh():
+  def __init__(self):
+    pass
+
+  def scatter_plot():
+    pass
+
+  def line_plot():
+    pass
+
+  def bar_graph():
+    pass
+
+  def ddd_graph():
+    pass
+
+  def render_graph():
+    pass

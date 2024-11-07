3fc0087c5afb0ba9b5d61b457c21e3479038c4e0
Alex Refermat <73394181+arefermat@users.noreply.github.com>
2024-11-07 12:08:48 -0600

    Update data-prep.py

--- data-prep.py
+++ data-prep.py
@@ -7,19 +7,16 @@ import numpy
 def prep_data_for_graph():
   pass
 
-def prep_data_for_graph(neruonsone, neuronstwo, denseamt, dense1, dense2, dense3, dense4, dense5, epochsize, batchsize, profit, profitprcntg, startingmoney, timetookrun, timeran, stocksymbol, interval):
+def add_row_to_table(neruonsone, neuronstwo, denseamt, dense1, dense2, dense3, dense4, dense5, epochsize, batchsize, profit, profitprcntg, startingmoney, timetookrun, timeran, stocksymbol, interval)
   with open("data/data.md", "a") as data:
     data.append(f"| {neuronsone} | {neuoronstwo} | {denseamt} | {dense1} | {dense2} | {dense3} | {dense4} | {dense5} | {epochsize} | {batchsize} | {profit} | {profitprcntg} | {startingmoney} | {timetookrun} | {timeran} | {stocksymbol} | {interval} |")
-
-def fetch_data():
-  pass
 
 class gragh():
   def __init__(self):
     pass
 
-  def scatter_plot():
-    pass
+  def scatter_plot(x, y):
+    mpl.scatter(x, y)
 
   def line_plot():
     pass

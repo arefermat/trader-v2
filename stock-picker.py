79b5acefceb782d22089aae3716b742485f6c316
Alex Refermat <73394181+arefermat@users.noreply.github.com>
2024-11-08 10:12:47 -0600

    Update stock-picker.py

--- stock-picker.py
+++ stock-picker.py
@@ -17,6 +17,17 @@ class logistic_regresion():
 
   def calculate_loss(y_prediction, y_actual):
     training_rotations = len(y_actual)
+    loss = -(1/training_rotations) * np.sum(y_actual * np.log(y_prediction) + (1 - y_actual) * log(1 - y_predicted))
+    return loss
 
-    loss = -1/training_rotations ( 
-    
+  def compute_gradients(x, y_actual, y_predicted):
+    m = x.shape[0]
+    dw = (1/m) * np.dot(x.T, (y_predicted - y_actual))
+    db = (1/m) * np.sum(y_predicted - y_actual)
+    return dw, db
+
+  def update_parameters(w, b, dw, db, learning_rate):
+    w -= learning_rate * dw
+    b -= learning_rate * db
+    return w, b
+

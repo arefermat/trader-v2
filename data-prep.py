2a42dfb19e991722195750184a84d2920de822f3
Alex Refermat <73394181+arefermat@users.noreply.github.com>
2024-11-11 19:40:52 -0600

    Update data-prep.py

--- data-prep.py
+++ data-prep.py
@@ -1,6 +1,6 @@
-import pandas
+import pandas as pd
 import schedule
-import matplotlib.pyplot as mpl
+import seaborn as sns
 import keyboard
 import numpy
 
@@ -11,15 +11,15 @@ def add_row_to_table(neruonsone, neuronstwo, denseamt, dense1, dense2, dense3, d
   with open("data/data.md", "a") as data:
     data.append(f"| {neuronsone} | {neuoronstwo} | {denseamt} | {dense1} | {dense2} | {dense3} | {dense4} | {dense5} | {epochsize} | {batchsize} | {profit} | {profitprcntg} | {startingmoney} | {timetookrun} | {timeran} | {stocksymbol} | {interval} |")
 
-class gragh():
+class graph():
   def __init__(self):
     pass
 
-  def scatter_plot(x, y):
-    mpl.scatter(x, y)
+  def scatter_plot(data_file):
+    sns.scatterplot(data_file)
 
   def line_plot(x, y):
-    mpl.plot(x, y)
+    pass
 
   def bar_graph():
     pass
@@ -28,4 +28,4 @@ class gragh():
     pass
 
   def render_graph():
-    mpl.show()
+    pass

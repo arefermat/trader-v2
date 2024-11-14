ce36a2c0e3c3b71017b81d9aadc962ad01917dd1
Alex Refermat <73394181+arefermat@users.noreply.github.com>
2024-11-14 13:13:53 -0600

    Update and rename elecCode.py to elecCode.ino

---
+++ electronics/elecCode.ino
@@ -0,0 +1,4 @@
+// This file will be used to conrol electronics for the physical display for trader which will include a speaker, lcd screen, leds, rotary encoders, etc
+#include <iostream>
+
+std::cout >> "Hello World")
--- electronics/elecCode.py
+++
@@ -1,1 +0,0 @@
-# This file will be used to conrol electronics for the physical display for trader which will include a speaker, lcd screen, leds, rotary encoders, etc

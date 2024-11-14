166f323ca03db0fb2f0d9baaf0f28cef24a105be
Alex Refermat <73394181+arefermat@users.noreply.github.com>
2024-11-14 13:24:57 -0600

    Update elecCode.ino

--- electronics/elecCode.ino
+++ electronics/elecCode.ino
@@ -1,4 +1,8 @@
 // This file will be used to conrol electronics for the physical display for trader which will include a speaker, lcd screen, leds, rotary encoders, etc
 #include <iostream>
 
-std::cout >> "Hello World")
+int rightButton = 9;
+int leftButton = 8;
+int redLed = 12;
+int greenLed = 11;
+

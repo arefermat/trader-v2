26ccf7c3c5d05b722110ac307d8812f714470ba7
Alex Refermat <73394181+arefermat@users.noreply.github.com>
2025-01-09 09:02:45 -0600

    Update dataPrep.py

--- dataPrep.py
+++ dataPrep.py
@@ -4,12 +4,20 @@ import seaborn as sns
 import keyboard
 import numpy
 
-def prep_data_for_graph():
-  pass
+def prep_data_for_graph(AP_TYPE, nameOfAI, neruonsone, neuronstwo, denseamt, dense1, dense2, dense3, dense4, dense5, epochsize, batchsize, profit, profitprcntg, startingmoney, timetookrunHours, timetookrunMins, timetookrunSecs, timeranHours, timeranMins, timeranSecs, stocksymbol, interval):
+  if AP_TYPE == "dt":
+      return nameOfAI, neruonsone, neuronstwo, denseamt, dense1, dense2, dense3, dense4, dense5, epochsize, batchsize, profit, profitprcntg, startingmoney, (f"{timetookrunHours};{timetookrunMins}:{timetookrunSecs}"), (f"{timeranHours};{timeranMins}:{timeranSec}"), stocksymbol, interval
+  elif AP_TYPE == "csv":
+    return nameOfAI, neruonsone, neuronstwo, denseamt, dense1, dense2, dense3, dense4, dense5, epochsize, batchsize, profit, profitprcntg, startingmoney, timetookrunHours, timetookrunMins, timetookrunSecs, timeranHours, timeranMins, timeranSecs, stocksymbol, interval
 
-def add_row_to_table(neruonsone, neuronstwo, denseamt, dense1, dense2, dense3, dense4, dense5, epochsize, batchsize, profit, profitprcntg, startingmoney, timetookrun, timeran, stocksymbol, interval)
-  with open("data/data.md", "a") as data:
-    data.append(f"| {neuronsone} | {neuoronstwo} | {denseamt} | {dense1} | {dense2} | {dense3} | {dense4} | {dense5} | {epochsize} | {batchsize} | {profit} | {profitprcntg} | {startingmoney} | {timetookrun} | {timeran} | {stocksymbol} | {interval} |")
+def add_row_to_table(nameOfAI, neruonsone, neuronstwo, denseamt, dense1, dense2, dense3, dense4, dense5, epochsize, batchsize, profit, profitprcntg, startingmoney, timetookrun, timeran, stocksymbol, interval)
+  with open("data/table.md", "a") as data:
+    data.append(f"| {nameOfAI} | {neuronsone} | {neuoronstwo} | {denseamt} | {dense1} | {dense2} | {dense3} | {dense4} | {dense5} | {epochsize} | {batchsize} | {profit} | {profitprcntg} | {startingmoney} | {timetookrun} | {timeran} | {stocksymbol} | {interval} |")
+
+def add_data_to_csv(nameOfAI, neruonsone, neuronstwo, denseamt, dense1, dense2, dense3, dense4, dense5, epochsize, batchsize, profit, profitprcntg, startingmoney, timetookrun, timeran, stocksymbol, interval)
+  with open("data/data.csv", "a") as data:
+    data.append(f"{nameOfAI}, {neuronsone}, {neuoronstwo}, {denseamt}, {dense1}, {dense2}, {dense3}, {dense4}, {dense5}, {epochsize}, {batchsize}, {profit}, {profitprcntg}, {startingmoney}, {timetookrun}, {timeran}, {stocksymbol}, {interval}")
+
 
 class graph():
   def __init__(self):

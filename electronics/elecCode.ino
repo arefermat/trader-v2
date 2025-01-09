39a73b1b8112ac50e1419a538cab1584b2833d68
Alex Refermat <73394181+arefermat@users.noreply.github.com>
2025-01-09 08:14:26 -0600

    Delete electronics directory

--- electronics/elecCode.ino
+++
@@ -1,8 +0,0 @@
-// This file will be used to conrol electronics for the physical display for trader which will include a speaker, lcd screen, leds, rotary encoders, etc
-#include <iostream>
-
-int rightButton = 9;
-int leftButton = 8;
-int redLed = 12;
-int greenLed = 11;
-
